f69732bbe9fb51d7b538c5986d4485fe51582670
Stefan Vojvodic <stefan.vojvodic@gamecredits.com>
2018-12-19 11:58:43 +0100

    add classes

--- src/theme.js
+++ src/theme.js
@@ -2,4 +2,7 @@ export const defaultTheme = {
   reel: 'react-reel__reel',
   group: 'react-reel__group',
   number: 'react-reel__number',
+  dot: 'react-reel__dot',
+  comma: 'react-reel__comma',
+  separator: 'react-reel__separator'
 };
